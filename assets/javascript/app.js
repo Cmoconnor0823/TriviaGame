89d1647b776510ad8407d5bd5c69618cc9d9f667
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-06-11 18:39:18 -0700

    Added full question array and cleared extra line spacing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ var quesOpt = [
      {
         question:"What posistion does Harry play on his Quidditch team?",
         choices:["Bludger", "Chaser","Seeker","Keeper"],
-        answer: "4",
+        answer: "Seeker",
     
     },
      {
@@ -52,17 +52,17 @@ $("#start-button").on("click", quesPost);
 function countDown() {
     secLeft--;
     run();
-    
+
     //  Show the number in the #show-number tag.
     $("#time-remaining").html("<h2>" + secLeft + "</h2>");
-    
-    
+
+
     //  Once number hits zero...
     if (secLeft === 0) {
-        
+
         //  ...run the stop function.
         stop();
-        
+
         //  Alert the user that time is up.
         alert("Time Up!");
         // display questions correct and questions incorrect
@@ -71,40 +71,43 @@ function countDown() {
     function stop() {
         clearInterval(timeLeftId);
     }
-    
+
     //  Execute the run function.
-        function run() {
-            clearInterval(timeLeftId);
-            timeLeftId = setInterval(countDown, 1000);
-        }
+    function run() {
+        clearInterval(timeLeftId);
+        timeLeftId = setInterval(countDown, 1000);
+    }
 }
-            function quesPost() {
-                //Start the countdown timer
-                countDown();
-                // Looping through the array of questions
-                //Post a true or false button for each question????
-            
-                for (var i = 0; i < quesOpt.length; i++) {
-            
-                    // Then dynamicaly generating HTML for each question in the array.
-                    // This code $("<div>") is to create the start and end tag. (<div></div>)
-                    var a = $("<div>");
-                    // Adding a class
-                    a.addClass("quesState");
-                    // Providing the question's text with a value of the questions objects  at index i
-                    a.text(quesOpt[i].question);
-                    $(".questions").append(a);
-                    // Adding the button to the HTML
-                    for (var j = 0; j < quesOpt[i].choices.length; j++){
-                     var b = $("<button>");
-                      b.addClass("btn btn-outline m-2 btn-lg img-responsive buttonS button");
-                      b.text(quesOpt[i].choices[j]);
-                      $(".questions").append(b);
-                    }
-                
-                }
-                //this correctly adds a button benith the question but stops posting further questions
-                //for (var i =0; i < ansOpt.length; i++){
-               // }
-                
-            }
+function quesPost() {
+    //Start the countdown timer
+    countDown();
+    // Looping through the array of questions
+    //Post a true or false button for each question????
+
+    for (var i = 0; i < quesOpt.length; i++) {
+
+        // Then dynamicaly generating HTML for each question in the array.
+        // This code $("<div>") is to create the start and end tag. (<div></div>)
+        var a = $("<div>");
+        // Adding a class
+        a.addClass("quesState");
+        // Providing the question's text with a value of the questions objects  at index i
+        a.text(quesOpt[i].question);
+        $(".questions").append(a);
+        // Adding the button to the HTML
+        for (var j = 0; j < quesOpt[i].choices.length; j++) {
+            var b = $("<button>");
+            b.addClass("btn btn-outline m-2 btn-lg img-responsive buttonS button");
+
+            b.text(quesOpt[i].choices[j]);
+            $(".questions").append(b);
+        }
+
+    }
+    //this correctly adds a button benith the question but stops posting further questions
+    //for (var i =0; i < ansOpt.length; i++){
+    // }
+
+}
+
+
